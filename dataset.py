7a06e9e7884658bada0fb5515099747b8f2f5064
smreosms13 <smreosms13@gamil.com>
2023-09-13 20:38:21 +0900

    feat : connect to mysql database using pymysql and secret keys of env

--- dataset.py
+++ dataset.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import pymysql
 from dbconfig import Settings
 
 settings = Settings()
@@ -9,10 +9,12 @@ db_user = settings.db_user
 db_password = settings.db_password
 db_db = settings.db_db
 
-connection = mysql.connector.connect(
-    host = db_host,
-    port = db_port,   
-    database = db_db,  
-    user = db_user,
-    password = db_password
-)
+class MysqlConnection :
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host = db_host,
+            port = db_port,
+            database = db_db,
+            user = db_user,
+            password = db_password
+        )
